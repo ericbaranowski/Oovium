649f4537528a20ce2eba4ca309b5969e6816dd0c
Joe Charlier <joe@aepryus.com>
2017-08-07 18:25:48 -0700

    further Localizable fixes

---
+++ Oovium/MessageHover.swift
@@ -0,0 +1,13 @@
+//
+//  MessageHover.swift
+//  Oovium
+//
+//  Created by Joe Charlier on 8/7/17.
+//  Copyright © 2017 Aepryus Software. All rights reserved.
+//
+
+import UIKit
+
+class MessageHover: Hover {
+	
+}
